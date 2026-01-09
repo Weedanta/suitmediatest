3596e6da433322c995858663958a314b662d6954
Weedanta <baguswedanta@student.ub.ac.id>
2026-01-10 00:08:07 +0800

    feat: add navigation list for header component

---
+++ shared/components/header/data/navList.ts
@@ -0,0 +1,32 @@
+export const navList = [
+  {
+    id: "Home",
+    title: "Home",
+    href: "/",
+  },
+  {
+    id: "Work",
+    title: "Work",
+    href: "/work",
+  },
+  {
+    id: "About",
+    title: "About",
+    href: "/about",
+  }, 
+  {
+    id: "Ideas",
+    title: "Ideas",
+    href: "/ideas",
+  }, 
+  {
+    id: "Careers",
+    title: "Careers",
+    href: "/careers",
+  },
+  {
+    id: "Contact",
+    title: "Contact",
+    href: "/contact",
+  }
+]
